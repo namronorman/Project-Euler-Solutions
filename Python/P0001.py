c333763dd408ae82b6d545fbef5e20e8639dca38
Norman Brown <jon.n.brown@gmail.com>
2019-09-25 14:57:00 -0400

    Completed problem 1 of Project Euler in Python.

---
+++ Python/P0001.py
@@ -0,0 +1,21 @@
+"""
+
+    Problem 1:
+
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
+if __name__ == "__main__":
+    def multiples_of(limit):
+        multiples_sum = 0
+
+        for i in range(1, limit):
+            if i%3 == 0 or i%5 == 0:
+                multiples_sum = multiples_sum + i
+
+        print(multiples_sum)
+
+    multiples_of(limit = 1000)
